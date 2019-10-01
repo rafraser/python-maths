38b55e33ec0a37b180ed9d5d2052d02d1cf458e9
FluffyXVI <fluffyxvi@gmail.com>
2019-10-01 10:18:07 +1000

    Sample programs for rendering animations

---
+++ mandelbrot/julia_animation.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from mandelbrot import Julia
+
+y_pos = 0.15
+x_start = -1.75
+x_end = 1.5
+frames = 120
+resolution = 512
+
+step = (x_end - x_start)/frames
+gif = []
+for i in range(frames):
+    print('Rendering frame ', i)
+    x_pos = x_start + step*i
+    j = Julia(complex(x_pos, y_pos), resolution)
+    gif.append(j.render(None, False))
+
+# Save the gif
+gif[0].save('julia.gif', format='GIF', append_images=gif[1:], save_all=True, duration=1/60, loop=0)
---
+++ mandelbrot/power_animation.py
@@ -0,0 +1,14 @@
+from PIL import Image
+from mandelbrot import Mandelbrot
+
+resolution = 512
+gif = []
+for i in range(120):
+    print('Rendering frame ', i)
+    p = 1 + 0.05*i
+    m = Mandelbrot(resolution, quality=20, x=0, y=0, radius=2, power=p)
+    m.set_palette(1 + 0.02*i, 1.5, 2 - 0.01*i)
+    gif.append(m.render(None, False))
+
+# Save the gif
+gif[0].save('powers.gif', format='GIF', append_images=gif[1:], save_all=True, duration=1/60, loop=0)
